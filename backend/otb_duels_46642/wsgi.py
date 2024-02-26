e45c6cbeb7fca36578d17fdd801e72aa4f2c9116
Crowdbotics <team@crowdbotics.com>
2024-02-26 17:35:23 +0000

    Initial commit

---
+++ backend/otb_duels_46642/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for otb_duels_46642 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "otb_duels_46642.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "otb-duels-46642",
+  url: "https://otb-duels-46642.botics.co",
+  api: "https://otb-duels-46642.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
